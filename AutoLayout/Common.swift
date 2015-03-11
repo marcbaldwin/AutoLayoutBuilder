e017d177e3ddf8003f4422d27421559d4b21119f
marcbaldwin <marc.baldwin88@gmail.com>
2015-03-11 16:03:46 +0000

    Add equal heights convenience method

--- AutoLayout/Common.swift
+++ AutoLayout/Common.swift
@@ -45,3 +45,12 @@ extension NSLayoutConstraint {
         self.init(item: view1, attribute: attr1, relatedBy: relation, toItem: view2, attribute: attr2, multiplier: multiplier, constant: c)
     }
 }
+
+public func equalHeights(views: UIView...) -> [NSLayoutConstraint] {
+    let firstView = views.first!
+    var constraints = [NSLayoutConstraint]()
+    for view in views[1..<views.count] {
+        constraints.append(NSLayoutConstraint(firstView, .Height, .Equal, view, .Height, 1, 0))
+    }
+    return constraints
+}
---
+++ AutoLayoutTests/Tests.swift
@@ -0,0 +1,35 @@
+//
+//  Tests.swift
+//  AutoLayout
+//
+//  Created by Marc Baldwin on 11/03/2015.
+//  Copyright (c) 2015 Marc Baldwin. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import XCTest
+
+class Tests: XCTestCase {
+
+    let view1 = UIView()
+    let view2 = UIView()
+    let view3 = UIView()
+
+    // MARK: Tests
+
+    func testEqualHeightsWithTwoViews() {
+        let constraints = equalHeights(view1, view2)
+        XCTAssertEqual([NSLayoutConstraint(view1, .Height, .Equal, view2, .Height, 1, 0)], constraints)
+    }
+
+    func testEqualHeightsWithThreeViews() {
+        let constraints = equalHeights(view1, view2, view3)
+        XCTAssertEqual([
+            NSLayoutConstraint(view1, .Height, .Equal, view2, .Height, 1, 0),
+            NSLayoutConstraint(view1, .Height, .Equal, view3, .Height, 1, 0)
+            ], constraints)
+    }
+
+}
