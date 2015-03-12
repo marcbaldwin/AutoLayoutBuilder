5a76c423f6046ef570f9645524880977ac2e4774
marcbaldwin <marc.baldwin88@gmail.com>
2015-03-12 23:09:30 +0000

    Add convenience notation for stacking views vertically

---
+++ AutoLayout/StackLayoutBuilder.swift
@@ -0,0 +1,55 @@
+//
+//  StackLayoutBuilder.swift
+//  AutoLayout
+//
+//  Created by Marc Baldwin on 12/03/2015.
+//  Copyright (c) 2015 Marc Baldwin. All rights reserved.
+//
+
+import UIKit
+
+public enum StackDirection {
+    case Vertical
+}
+
+public class StackBuilder {
+    var views: [UIView]
+    var constants: [CGFloat]
+    public init(views: UIView...) {
+        self.views = views
+        self.constants = [0]
+    }
+}
+
+infix operator ~ { associativity left precedence 90 }
+
+public func ~(lhs: StackDirection, rhs: StackBuilder) -> [NSLayoutConstraint] {
+    var constraints = [NSLayoutConstraint]()
+    let views = rhs.views
+
+    for i in 1..<views.count {
+        constraints.append(NSLayoutConstraint(views[i], .Top, .Equal, views[i-1], .Bottom, 1, rhs.constants[i-1]))
+    }
+    return constraints
+}
+
+public func |(lhs: UIView, rhs: UIView) -> StackBuilder {
+    return StackBuilder(views: lhs, rhs)
+}
+
+public func |(lhs: StackBuilder, rhs: UIView) -> StackBuilder {
+    lhs.views.append(rhs)
+    lhs.constants.append(0)
+    return lhs
+}
+
+public func |(lhs: StackBuilder, rhs: CGFloat) -> StackBuilder {
+    lhs.constants[lhs.constants.count-1] = rhs
+    return lhs
+}
+
+public func |(lhs: UIView, rhs: CGFloat) -> StackBuilder {
+    let stackBuilder = StackBuilder(views: lhs)
+    stackBuilder.constants[stackBuilder.constants.count-1] = rhs
+    return stackBuilder
+}
---
+++ AutoLayoutTests/StackLayoutTests.swift
@@ -0,0 +1,40 @@
+//
+//  Created by Marc Baldwin on 11/03/2015.
+//  Copyright (c) 2015 Marc Baldwin. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import XCTest
+
+class StackLayoutTests: XCTestCase {
+
+    let view1 = UIView()
+    let view2 = UIView()
+    let view3 = UIView()
+
+    func testStackLayoutWithTwoViews() {
+        let constraints = .Vertical ~ view1 | view2
+        XCTAssertEqual([NSLayoutConstraint(view2, .Top, .Equal, view1, .Bottom, 1, 0)], constraints)
+    }
+
+    func testStackLayoutWithThreeViews() {
+        let constraints = .Vertical ~ view1 | view2 | view3
+        XCTAssertEqual([
+            NSLayoutConstraint(view2, .Top, .Equal, view1, .Bottom, 1, 0),
+            NSLayoutConstraint(view3, .Top, .Equal, view2, .Bottom, 1, 0)], constraints)
+    }
+
+    func testStackLayoutWithTwoViewsAndConstant() {
+        let constraints = .Vertical ~ view1 | 10 | view2
+        XCTAssertEqual([NSLayoutConstraint(view2, .Top, .Equal, view1, .Bottom, 1, 10)], constraints)
+    }
+
+    func testStackLayoutWithThreeViewsAndConstant() {
+        let constraints = .Vertical ~ view1 | 10 | view2 | view3
+        XCTAssertEqual([
+            NSLayoutConstraint(view2, .Top, .Equal, view1, .Bottom, 1, 10),
+            NSLayoutConstraint(view3, .Top, .Equal, view2, .Bottom, 1, 0)], constraints)
+    }
+}
