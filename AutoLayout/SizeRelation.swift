0d36525eb5783f14015faa4c312a2667c2457f87
marcbaldwin <marc.baldwin88@gmail.com>
2015-03-23 20:39:07 +0000

    Rename SizeConstraintBuilder to SizeRelation

---
+++ AutoLayout/SizeRelation.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+public enum SizeAttribute {
+    case Size
+}
+
+public struct SizeRelation {
+    let views: [UIView]
+    let attribute: SizeAttribute
+    let multiplier: CGFloat
+    let constant: CGFloat
+}
+
+// MARK: Operator Overloads
+
+public func ==(lhs: SizeRelation, rhs: SizeRelation) -> [NSLayoutConstraint] {
+    var constraints = [NSLayoutConstraint]()
+    for view in lhs.views {
+        constraints.append(NSLayoutConstraint(view, .Width, .Equal, rhs.views.first!, .Width, rhs.multiplier, rhs.constant))
+        constraints.append(NSLayoutConstraint(view, .Height, .Equal, rhs.views.first!, .Height, rhs.multiplier, rhs.constant))
+    }
+    return constraints
+}
+
+public func *(lhs: SizeRelation, rhs: CGFloat) -> SizeRelation {
+    return SizeRelation(views: lhs.views, attribute: lhs.attribute, multiplier: lhs.multiplier * rhs, constant: lhs.constant)
+}
+
+public func /(lhs: SizeRelation, rhs: CGFloat) -> SizeRelation {
+    return lhs * (1/rhs)
+}
+
+public func +(lhs: SizeRelation, rhs: CGFloat) -> SizeRelation {
+    return SizeRelation(views: lhs.views, attribute: lhs.attribute, multiplier: lhs.multiplier, constant: lhs.constant + rhs)
+}
+
+public func -(lhs: SizeRelation, rhs: CGFloat) -> SizeRelation {
+    return lhs + (-rhs)
+}
+
+// MARK: Extensions
+
+public extension UIView {
+
+    subscript(dimensionAttribute: SizeAttribute) -> SizeRelation {
+        return SizeRelation(views: [self], attribute: dimensionAttribute, multiplier: 1, constant: 0)
+    }
+}
+
+public extension Views {
+
+    subscript(dimensionAttribute: SizeAttribute) -> SizeRelation {
+        return SizeRelation(views: views, attribute: dimensionAttribute, multiplier: 1, constant: 0)
+    }
+}
