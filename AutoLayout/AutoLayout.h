2e339496cc3084f44451d499b5c4d3cc41c6505f
marcbaldwin <marc.baldwin88@gmail.com>
2015-07-01 01:16:50 +0100

    Tidy header and increment version.

--- AutoLayout/AutoLayout.h
+++ AutoLayout/AutoLayout.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double AutoLayoutVersionNumber;
 
 //! Project version string for AutoLayout.
 FOUNDATION_EXPORT const unsigned char AutoLayoutVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AutoLayout/PublicHeader.h>
-
-
