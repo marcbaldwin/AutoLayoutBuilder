37b0f780585b0f1f465c415a4e62bbfec711204b
marcbaldwin <marc.baldwin88@gmail.com>
2015-07-02 12:23:46 +0100

    Make aspect ratio tests more descriptive

--- AutoLayoutTests/AspectRatioTests.swift
+++ AutoLayoutTests/AspectRatioTests.swift
@@ -5,12 +5,12 @@ class AspectRatioTests: ALBTestCase {
 
     let ratio: CGFloat = 16/9
 
-    func testDeclareAspectRatioForSingleView() {
+    func testDeclareAspectRatioOfViewEqualToConstantRatio() {
         let constraints = view1[.AspectRatio] == ratio
         XCTAssertEqual(NSLayoutConstraint(view1, .Width, .Equal, view1, .Height, ratio, 0), constraints[0])
     }
 
-    func testDeclareAspectRatioForManyViews() {
+    func testDeclareAspectRatioOfMultipleViewsEqualToConstantRatio() {
         let constraints = Group(view1, view2)[.AspectRatio] == ratio
         XCTAssertEqual(NSLayoutConstraint(view1, .Width, .Equal, view1, .Height, ratio, 0), constraints[0])
         XCTAssertEqual(NSLayoutConstraint(view2, .Width, .Equal, view2, .Height, ratio, 0), constraints[1])
